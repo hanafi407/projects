80ec9d39105ebf434f1080a69446c5d4ebbe61fe
hanafi407 <menabung08@gmail.com>
2021-10-15 09:39:10 +0700

    update accordion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import Projects from "./projects";
-
-
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className='container'>
-      <Projects />
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
